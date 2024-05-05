74d709ffbf56d2e40567cb2531cbe95c6b8fe8d7
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-05-06 00:16:29 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; // Import CSS styles
+
+function App() {
+  return (
+    <div className="App">
+      <div className="circle"></div>
+    </div>
+  );
+}
+
+export default App;
