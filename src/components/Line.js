07fb4093ca40efa2a1e6c55e98e7313b0a05a3f2
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-06-02 00:03:43 +0200

    Added time seperators

--- src/components/Line.js
+++ src/components/Line.js
@@ -26,14 +26,46 @@ const Line = () => {
     return () => clearInterval(timerId); // Cleanup on component unmount
   }, []);
 
-  return (
+
+    const segments = [
+    { title: 'Work', start: '08:00', end: '13:00' },
+    { title: 'Break', start: '13:00', end: '14:00' },
+    { title: 'Work', start: '14:00', end: '19:00' },
+  ];
+
+ const calculatePosition = (time) => {
+    const [hours, minutes] = time.split(':').map(Number);
+    const totalMinutes = hours * 60 + minutes;
+    return (totalMinutes / (24 * 60)) * 100;
+  };
+
+  const contents = (
     <div className="lineContainer">
       <div className="verticalBar"></div>
-      <div className="line"></div>
+      <div className="line">
+        {segments.map((segment, index) => (
+          <React.Fragment key={index}>
+            <div
+              className="segmentSeparator"
+              style={{ left: `${calculatePosition(segment.start)}%` }}
+            >
+              <div className="timeLabel">{segment.start}</div>
+            </div>
+            <div
+              className="segmentSeparator"
+              style={{ left: `${calculatePosition(segment.end)}%` }}
+            >
+              <div className="timeLabel">{segment.end}</div>
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
       <div className="verticalBar"></div>
       <img src={triangleImage} className="triangle" style={{ left: leftPosition }} alt="Triangle pointer" />
     </div>
   );
+
+  return contents;
 };
 
 export default Line;
