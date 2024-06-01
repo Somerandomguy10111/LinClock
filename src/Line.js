9c7f2025aa14a1fd9e4be4dec58c21d0bd600fba
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-06-01 20:08:08 +0200

    Implemented triangle and line

--- src/Line.js
+++ src/Line.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Line.css';
+import triangleImage from './triangle.png';  // Make sure the path is correct
+
 
 const Line = () => (
-  <div className="line"></div>
+  <div className="lineContainer">
+    <img src={triangleImage} className="triangle" alt="Triangle pointer" />
+    <div className="verticalBar"></div>
+    <div className="line"></div>
+    <div className="verticalBar"></div>
+  </div>
 );
 
 export default Line;
