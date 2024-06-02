b84c6ece4b8754d27feb22f75cfe7bdb992fa228
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-06-02 06:56:39 +0200

    Modularized line.js

--- src/components/Line.js
+++ src/components/Line.js
@@ -3,59 +3,75 @@ import './Line.css';
 import triangleImage from 'src/img/triangle.png';  // Make sure the path is correct
 
 
-const Line = ({ segments }) => {
-  const calculatePositionFromDate = (date) => {
-    const secondsSinceMidnight = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
-    const secondsInADay = 86400;
-    return `${(secondsSinceMidnight / secondsInADay) * 100}%`;
-  };
-
-  const [leftPosition, setLeftPosition] = useState(calculatePositionFromDate(new Date()));
-
-  useEffect(() => {
-    const updatePosition = () => {
-      setLeftPosition(calculatePositionFromDate(new Date()));
-    };
-
-    const timerId = setInterval(updatePosition, 1000);
-
-    return () => clearInterval(timerId);
-  }, []);
+const SegmentSeparator = ({ time, position }) => {
+  return (
+    <div className="segmentSeparator" style={{ left: `${position}%` }}>
+      <div className="timeLabel">{time}</div>
+    </div>
+  );
+};
 
 
- const calculatePosition = (time) => {
-    const [hours, minutes] = time.split(':').map(Number);
-    const totalMinutes = hours * 60 + minutes;
-    return (totalMinutes / (24 * 60)) * 100;
-  };
-
-  const contents = (
-    <div className="lineContainer">
-      <div className="verticalBar"></div>
-      <div className="line">
+const Timeline = ({ segments, calculatePosition }) => {
+    const contents = (
+    <div className="line">
         {segments.map((segment, index) => (
-          <React.Fragment key={index}>
-            <div
-              className="segmentSeparator"
-              style={{ left: `${calculatePosition(segment.start)}%` }}
-            >
-              <div className="timeLabel">{segment.start}</div>
-            </div>
-            <div
-              className="segmentSeparator"
-              style={{ left: `${calculatePosition(segment.end)}%` }}
-            >
-              <div className="timeLabel">{segment.end}</div>
-            </div>
-          </React.Fragment>
+            <React.Fragment key={index}>
+                <SegmentSeparator time={segment.start} position={calculatePosition(segment.start)} />
+                <SegmentSeparator time={segment.end} position={calculatePosition(segment.end)} />
+            </React.Fragment>
         ))}
-      </div>
-      <div className="verticalBar"></div>
-      <img src={triangleImage} className="triangle" style={{ left: leftPosition }} alt="Triangle pointer" />
     </div>
   );
 
-  return contents;
+  return contents
+};
+
+const Line = ({ segments }) => {
+    const pos = calculatePositionFromDate(new Date())
+    const [leftPosition, setLeftPosition] = useState(pos);
+
+    function calculatePositionFromDate(date) {
+        const secondsSinceMidnight = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+        const secondsInADay = 86400;
+        // const secondsSinceMidnight = date.getSeconds();
+        // const secondsInADay = 60;
+        return `${(secondsSinceMidnight / secondsInADay) * 100}%`;
+    };
+
+    function updatePosition() {
+        setLeftPosition(calculatePositionFromDate(new Date()));
+    };
+
+
+    function registerUpdate() {
+        const timerId = setInterval(updatePosition, 1000)
+        return () => clearInterval(timerId)
+    };
+
+    useEffect(registerUpdate);
+
+
+    const calculatePosition = (time) => {
+        const [hours, minutes] = time.split(':').map(Number);
+        const totalMinutes = hours * 60 + minutes;
+        return (totalMinutes / (24 * 60)) * 100;
+    };
+
+
+
+
+    const contents = (
+        <div className="lineContainer">
+            <div className="verticalBar"></div>
+            <Timeline segments={segments} calculatePosition={calculatePosition} />
+            <div className="verticalBar"></div>
+            <img src={triangleImage} className="triangle" style={{left: leftPosition}} alt="Triangle pointer"/>
+        </div>
+    );
+
+
+    return contents;
 };
 
 export default Line;
