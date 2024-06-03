7fa0ebf0269c7b7191cc27410c2c04c4bd005501
Daniel Hollarek <daniel.hollarek@googlemail.com>
2024-06-03 23:50:56 +0200

    Added colors to segements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import Line from 'src/components/Line';
 import DateDisplay from 'src/components/DateDisplay';
@@ -6,10 +5,10 @@ import DateDisplay from 'src/components/DateDisplay';
 
 function App() {
     const segments = [
-    { title: 'Work', start: '08:00', end: '13:00' },
-    { title: 'Break', start: '13:00', end: '14:00' },
-    { title: 'Work', start: '14:00', end: '19:00' },
-  ];
+        { title: 'Work', start: '08:00', end: '13:00', color: '#3333FF' },
+        { title: 'Break', start: '13:00', end: '14:00', color: '#00FF00' },
+        { title: 'Work', start: '14:00', end: '19:00', color: '#3333FF' },
+    ];
 
   const content = (
     <div className="App">
